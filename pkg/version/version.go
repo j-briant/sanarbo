d5288da772ab684f076cb6036aca8274d406b9f2
cgil <lao.tseu.is.alive@gmail.com>
2023-02-22 15:50:07 +0100

    added GitHub action for building container image

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "sanarbo"
 	AppSnake   = "sanarbo"
-	VERSION    = "0.0.1"
+	VERSION    = "0.0.2"
 	REPOSITORY = "github.com/lao-tseu-is-alive/sanarbo"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
