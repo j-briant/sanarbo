ccc981226a73971f709e9627960519ccc5216fa7
Maurice Pittet <maurice.pittet@lausanne.ch>
2023-03-28 09:06:50 +0200

    The geom column is now of type geometry

--- pkg/trees/tree_sql.go
+++ pkg/trees/tree_sql.go
@@ -8,7 +8,7 @@ const (
 
 	treesGet = `
 	SELECT id, name, description, external_id, is_active, inactivation_time, inactivation_reason, comment, is_validated, id_validator,
-			create_time, creator, last_modification_time, last_modification_user, geom, tree_attributes
+			create_time, creator, last_modification_time, last_modification_user, ST_AsText(geom) as geom, tree_attributes
 	FROM tree_mobile
 	WHERE id = $1`
 	
@@ -21,7 +21,7 @@ const (
 	treesCreate = `
 	INSERT INTO tree_mobile
 	(name, description, external_id, is_active, comment, create_time, creator, geom, tree_attributes) 
-	VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, $6, $7, $8)
+	VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, $6, ST_GeomFromText($7, 2056), $8)
 	RETURNING id;`
 	
 	treesUpdate = `
@@ -37,7 +37,7 @@ const (
 		id_validator			= $9,
 		last_modification_time 	= CURRENT_TIMESTAMP,
 		last_modification_user	= $10,
-		geom					= $11,
+		geom					= ST_GeomFromText($11, 2056),
 		tree_attributes			= $12
 	WHERE id = $13;`
 
@@ -48,24 +48,25 @@ const (
 	treesIsActive = "SELECT isactive FROM tree_mobile WHERE id = $1;"
 
 	treesCreateTable = `
-	CREATE TABLE IF NOT EXISTS tree_mobile
+	CREATE TABLE IF NOT EXISTS ${TABLE_NAME}
 	(
-		id        				serial    			CONSTRAINT tree_mobile_pk   primary key,
-		name					text	not null	constraint name_min_length check (length(btrim(name)) > 2),
-		description				text				constraint description_min_length check (length(btrim(description)) > 2),
-		external_id				int,
-		is_active				boolean default true not null,
-		inactivation_time		timestamp,
-		inactivation_reason    	text,
-		comment                	text,
-		is_validated			boolean default false,
-		id_validator			int,
-		create_time				timestamp default now() not null,
-		creator					integer	not null,
-		last_modification_time	timestamp,
-		last_modification_user	integer,
-		geom					text	not null,
-		tree_attributes			jsonb	not null
+	  id                      serial            CONSTRAINT tree_mobile_pk   primary key,
+	  name                    text  not null constraint name_min_length check (length(btrim(name)) > 2),
+	  description             text           constraint description_min_length check (length(btrim(description)) > 2),
+	  external_id             int,
+	  is_active               boolean default true not null,
+	  inactivation_time       timestamp,
+	  inactivation_reason     text,
+	  comment                 text,
+	  is_validated            boolean default false,
+	  id_validator            int,
+	  create_time             timestamp default now() not null,
+	  creator                 integer  not null,
+	  last_modification_time  timestamp,
+	  last_modification_user  integer,
+	  geom                    geometry(Point,2056)  not null,
+	  tree_attributes         jsonb not null
 	);
-	comment on table tree_mobile is 'tree_mobile is the main table of the sanarbo application';`
+	ALTER TABLE tree_mobile OWNER TO postgres;
+	COMMENT ON TABLE tree_mobile is 'tree_mobile is the main table of the sanarbo application';`
 )
