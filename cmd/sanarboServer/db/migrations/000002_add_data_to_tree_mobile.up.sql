7834ff168c5b0208f0ae2de0670830ccd1bb2136
Maurice Pittet <maurice.pittet@lausanne.ch>
2023-03-31 11:01:57 +0200

    Added more data rows and down operation

--- cmd/sanarboServer/db/migrations/000002_add_data_to_tree_mobile.up.sql
+++ cmd/sanarboServer/db/migrations/000002_add_data_to_tree_mobile.up.sql
@@ -1,3 +1,7 @@
 INSERT INTO tree_mobile
 (name, description, is_active, create_time, creator, geom, tree_attributes) 
 VALUES ('MyNewTree', 'Test de création', 't', CURRENT_TIMESTAMP, 999, ST_GeomFromText('POINT(2538221 1152372)', 2056), '{"idvalidation":1, "etatsanitairerem":"Rien à signaler"}');
+
+INSERT INTO tree_mobile
+(name, description, is_active, create_time, creator, geom, tree_attributes) 
+VALUES ('Mon bel arbre', 'création d''un nouvel arbre', 't', CURRENT_TIMESTAMP, 999, ST_GeomFromText('POINT(2538221 1152372)', 2056), '{"idvalidation":2, "enracinairerem":"En ordre"}');
